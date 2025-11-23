6f3af43260eb431167780b1e75d67877c1d4d4df
Влад <vlados@example.com>
2025-11-23 21:36:43 +0200

    Update anim

--- js/script.js
+++ js/script.js
@@ -12,3 +12,26 @@ for (let i = 0; i < snowCount; i++) {
 
     document.body.appendChild(snow);
 }
+
+const faders = document.querySelectorAll('#Anim');
+
+
+const appearOptions = {
+  threshold: 0.1, 
+  rootMargin: "0px 0px -50px 0px" 
+};
+
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.add('show'); 
+    observer.unobserve(entry.target); 
+  });
+}, appearOptions);
+
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
